5a55a44ad8df16bb608c77bb56b39c2c65efa00f
Mukama Steven <stevenmukama1@gmail.com>
2023-03-18 14:55:58 +0200

    Add-associations

--- student.rb
+++ student.rb
@@ -1,4 +1,8 @@
+require './person'
+
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -6,5 +10,10 @@ class Student < Person
 
   def play_honky
     '¯\\(ツ)/¯'
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 end
