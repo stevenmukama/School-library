c3576b4ed06241fdbfdd05b439ce87310f5aff0a
Mukama Steven <stevenmukama1@gmail.com>
2023-03-22 09:30:27 +0200

    Fix another linter error

--- rental.rb
+++ rental.rb
@@ -4,10 +4,10 @@ class Rental
     @book = book
     @person = person
 
-    if @person && @book # Check that both @person and @book are not nil
-      @person.rentals << self
-      @book.rentals << self
-    end
+    return unless @person && @book # Use a guard clause to check that both @person and @book are not nil
+
+    @person.rentals << self
+    @book.rentals << self
   end
 
   attr_accessor :date, :book, :person
